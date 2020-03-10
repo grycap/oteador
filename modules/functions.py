4526eafe8eb2c9af53082baf98782367fb91574e
Crireca <45510666+Crireca@users.noreply.github.com>
2020-03-10 20:53:53 +0100

    Update functions.py

--- modules/functions.py
+++ modules/functions.py
@@ -19,23 +19,25 @@ def main( ):
         8.Quit
         """)
         ans=input("Elige una opcion: ")
-        if ans=="1":
-            state=input("Estado (running,stopped,..): ")
-            getInstancesByStateEC2(state)
-        elif ans=="2":
-            state=input("Estado (running,terminated,..): ")
-            getInstancesByStateRDS(state)
-        elif ans=="3":
-            getElasticLoadBalancing()
-        elif ans=="4":
-            getAutoScallingGroups()
-        elif ans=="5":
-            getElasticIP()
-        elif ans=="6":
-            getAllInstancesEC2()
-        elif ans=="7":
-            getAllInstancesRDS()
-        elif ans=="8":
+        if ans==1:
+            state=raw_input("Estado (running,stopped,..): ")
+            print_pretty(getInstancesByStateEC2(state))
+        elif ans==2:
+            state=raw_input("Estado (running,terminated,..): ")
+            print_pretty(getInstancesByStateRDS(state))
+        elif ans==3:
+            print_pretty(getElasticLoadBalancing())
+        elif ans==4:
+            print_pretty(getAutoScallingGroups())
+        elif ans==5:
+            print_pretty(getElasticIP())
+        elif ans==6:
+            print_pretty(getAllInstancesEC2())
+        elif ans==7:
+            print_pretty(getAllInstancesRDS())
+        elif ans==8:
+            print_pretty(getBuckets())
+        elif ans==9:
             ans = None
         else:
             print("\n Opcion incorrecta")
@@ -84,6 +86,32 @@ def getInfoInstanceEC2(instance,owner):
     print_instance = { "Id:" : InstanceId, "Type" : InstanceType , "State" : State, "Private IP" : PrivateIpAddress,"Public IP" : PublicIpAddress, "Launch time" : LaunchTime, "Owner": owner }
     return print_instance
 
+def getInfoElasticLoadBalancer(elb):
+    
+    if 'LoadBalancerName' in elb:
+        LoadBalancerName=elb['LoadBalancerName']
+    else:
+        LoadBalancerName=''
+
+    if 'DNSName' in elb:
+        DNSName=elb['DNSName']
+    else:
+        DNSName=''
+
+    if 'State' in elb:
+        State=elb['State']['Code']
+    else:
+        State=''
+
+    if 'CreatedTime' in elb:
+        CreatedTime=json.dumps(elb['CreatedTime'],default = myconverter)
+    else:
+        CreatedTime=''
+
+    print_instance = { "LoadBalancerName:" : LoadBalancerName, "DNSName" : DNSName , "State" : State,
+     "CreatedTime" : CreatedTime }
+    return print_instance
+
 def getInfoInstanceRDS(dbinstance):
 
     if 'DBInstanceIdentifier' in dbinstance:
@@ -105,8 +133,13 @@ def getInfoInstanceRDS(dbinstance):
         Engine=dbinstance['Engine']
     else :
         Engine=''
+
+    if 'InstanceCreateTime' in dbinstance:
+        InstanceCreateTime=json.dumps(dbinstance['InstanceCreateTime'],default = myconverter)
+    else :
+        InstanceCreateTime=''
     
-    print_instance = { "DBInstanceIdentifierId:" : DBInstanceIdentifier, "DBInstanceClass" : DBInstanceClass , "DBInstanceStatus" : DBInstanceStatus, "Engine" : Engine }
+    print_instance = { "DBInstanceIdentifierId:" : DBInstanceIdentifier, "DBInstanceClass" : DBInstanceClass , "DBInstanceStatus" : DBInstanceStatus, "Engine" : Engine, "Intance Create Time" : InstanceCreateTime }
     return print_instance
 
 def getInfoElasticIP(elasticip):
@@ -147,7 +180,7 @@ def getInfoAutoScallingGroups(asg):
 def getAllInstancesEC2():
     returned = []
     
-    client = boto3.client('ec2')
+    client = boto3.client('ec2',region_name='us-east-1')
     response = client.describe_instances()
     reservations = response['Reservations']
     for reservation in reservations:
@@ -164,7 +197,7 @@ def getAllInstancesEC2():
                 
 def getBuckets():
     returned=[]
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource('s3',region_name='us-east-1')
     for bucket in s3.buckets.all():
         returned.append(bucket.name)
     return returned
@@ -172,7 +205,7 @@ def getBuckets():
 
 def getInstancesByStateEC2(state):
     returned = []
-    client = boto3.client('ec2')
+    client = boto3.client('ec2',region_name='us-east-1')
     response = client.describe_instances(
         Filters=[
         {
@@ -183,6 +216,7 @@ def getInstancesByStateEC2(state):
         }
         ]
     )
+    
     reservations = response['Reservations']
     for reservation in reservations:
         instances = reservation['Instances']
@@ -191,16 +225,14 @@ def getInstancesByStateEC2(state):
             for tag in instance['Tags']:
                 if 'owner' in tag['Key']:
                     owner = tag['Value']
-                #print(json.dumps(getInfoInstanceEC2(instance,owner),default = myconverter))
-                returned.append(getInfoInstanceEC2(instance,owner))
+                    returned.append(getInfoInstanceEC2(instance,owner))
     
     return returned
 
 def getAllInstancesRDS():
     returned = []
-    client = boto3.client('rds')
+    client = boto3.client('rds',region_name='us-east-1')
     response = client.describe_db_instances()
-    
     dbinstances = response['DBInstances']
     
     for dbinstance in dbinstances:
@@ -210,7 +242,7 @@ def getAllInstancesRDS():
 
 def getInstancesByStateRDS(state):
     returned = []
-    client = boto3.client('rds')
+    client = boto3.client('rds',region_name='us-east-1')
     response = client.describe_db_instances()
     
     dbinstances = response['DBInstances']
@@ -222,17 +254,17 @@ def getInstancesByStateRDS(state):
 
 def getElasticLoadBalancing():
     returned = []
-    client = boto3.client('elb')
+    client = boto3.client('elbv2',region_name='us-east-1')
     response = client.describe_load_balancers()
     
-    list_elb = response['LoadBalancerDescriptions']
+    list_elb = response['LoadBalancers']
     for elb in list_elb:
-        print_pretty(elb)
+        returned.append(getInfoElasticLoadBalancer(elb))
     return returned
     
 def getAutoScallingGroups():
     returned = []
-    client = boto3.client('autoscaling')
+    client = boto3.client('autoscaling',region_name='us-east-1')
     response = client.describe_auto_scaling_groups()
 
     list_asg = response['AutoScalingGroups']
@@ -242,7 +274,7 @@ def getAutoScallingGroups():
 
 def getElasticIP():
     returned = []
-    client = boto3.client('ec2')
+    client = boto3.client('ec2',region_name='us-east-1')
     response = client.describe_addresses()
     
     list_eIP = response['Addresses']
